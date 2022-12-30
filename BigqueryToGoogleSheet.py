c9b0742a262f2fa2ec39ad286e0572f8ffbdca70
Herlihry <121634083+Herlihry@users.noreply.github.com>
2022-12-30 12:14:43 -0500

    Update BigqueryToGoogleSheet.py

--- BigqueryToGoogleSheet.py
+++ BigqueryToGoogleSheet.py
@@ -1,33 +1,45 @@
-# Import the necessary libraries
-import pandas as pd
-from google.cloud import bigquery
+# 1. Set up a project in the Google Cloud Platform and enable the BigQuery and Google Sheets API.
+# 2. Install the required libraries:
+#    pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client pandas gspread gspread_dataframe
+
+# 3. Authenticate your script to access the API using a service account key file or by using OAuth 2.0.
 from google.oauth2.credentials import Credentials
 
-# Set your BigQuery project ID and dataset ID
-project_id = 'your-project-id'
-dataset_id = 'your-dataset-id'
+creds = Credentials.from_service_account_file("service_account_key.json")
 
-# Set your Google Sheets ID and sheet name
-sheets_id = 'your-sheets-id'
-sheet_name = 'Sheet1'
+# 4. Use the `bigquery` library to run a query and store the results in a Pandas DataFrame.
+from google.cloud import bigquery
+import pandas as pd
 
-# Authenticate your Google Cloud account
-credentials = Credentials.from_service_account_file('path/to/service_account.json')
+# Construct a BigQuery client object.
+client = bigquery.Client()
 
-# Connect to BigQuery
-client = bigquery.Client(project=project_id, credentials=credentials)
+# Set up the query.
+query = """
+    SELECT *
+    FROM `bigquery-public-data.samples.natality`
+    WHERE year > 2000
+    LIMIT 1000
+"""
 
-# Set the query string
-query_string = 'SELECT * FROM `{}.{}`'.format(dataset_id, 'your-table-name')
+# Run the query and store the results in a DataFrame.
+df = client.query(query).to_dataframe()
 
-# Query the data
-query_job = client.query(query_string)
-df = query_job.to_dataframe()
+# 5. Use the `gspread` library to authenticate and access the Google Sheets API.
+import gspread
 
-# Export the data to Google Sheets
-service = build('sheets', 'v4', credentials=credentials)
+# Authenticate using the service account key file.
+client = gspread.authorize(creds)
 
-# Clear the existing data in the sheet
-sheet = service.spreadsheets().get(spreadsheetId=sheets_id).execute()
-sheet_id = sheet['sheets'][0]['properties']['sheetId']
-request = {
+# 6. Use the `gspread` library to create a new Google Sheet or open an existing one.
+# Create a new Google Sheet.
+sheet = client.create("My Sheet")
+
+# Or open an existing Google Sheet.
+sheet = client.open("My Sheet")
+
+# 7. Use the `gspread_dataframe` library to export the DataFrame to the Google Sheet.
+import gspread_dataframe as gd
+
+# Export the DataFrame to the Google Sheet.
+gd.set_with_dataframe(sheet.worksheet("Sheet1"), df)
